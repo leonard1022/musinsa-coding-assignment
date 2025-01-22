00c52e41bb032f1b98e5a4d59c2fec1af75cec5f
is.joo <dlstlrl8704@gmail.com>
2025-01-22 14:20:01 +0900

    first commit

---
+++ src/main/kotlin/com/musinsa/musinsacodingassignment/MusinsaCodingAssignmentApplication.kt
@@ -0,0 +1,11 @@
+package com.musinsa.musinsacodingassignment
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MusinsaCodingAssignmentApplication
+
+fun main(args: Array<String>) {
+    runApplication<MusinsaCodingAssignmentApplication>(*args)
+}
---
+++ src/test/kotlin/com/musinsa/musinsacodingassignment/MusinsaCodingAssignmentApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.musinsa.musinsacodingassignment
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MusinsaCodingAssignmentApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
