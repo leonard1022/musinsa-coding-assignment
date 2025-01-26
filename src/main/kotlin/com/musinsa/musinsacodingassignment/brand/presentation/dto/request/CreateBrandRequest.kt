36ba49dee6a154a087e5a025df94ce9f2ae51eb3
leonard1022 <dlstlrl8704@gmail.com>
2025-01-26 23:00:08 +0900

    Update: add validation for brand request dto

--- src/main/kotlin/com/musinsa/musinsacodingassignment/brand/presentation/dto/request/CreateBrandRequest.kt
+++ src/main/kotlin/com/musinsa/musinsacodingassignment/brand/presentation/dto/request/CreateBrandRequest.kt
@@ -1,9 +1,10 @@
 package com.musinsa.musinsacodingassignment.brand.presentation.dto.request
 
 import com.musinsa.musinsacodingassignment.brand.service.vo.CreateBrandVO
+import jakarta.validation.constraints.NotBlank
 
 data class CreateBrandRequest(
-    val name: String
+    @NotBlank val name: String
 )
 
 fun CreateBrandRequest.toVO() = CreateBrandVO(
