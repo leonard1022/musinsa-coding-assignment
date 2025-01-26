8025240b5f5bfd7bea844b2ecce3aa24e82f601a
leonard1022 <dlstlrl8704@gmail.com>
2025-01-26 13:20:28 +0900

    Update: modified brand controller requestmapping

--- src/main/kotlin/com/musinsa/musinsacodingassignment/brand/presentation/BrandController.kt
+++ src/main/kotlin/com/musinsa/musinsacodingassignment/brand/presentation/BrandController.kt
@@ -13,7 +13,8 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
-@RestController("/brands")
+@RestController
+@RequestMapping("/brands")
 class BrandController(
     private val brandService: BrandService
 ) {
