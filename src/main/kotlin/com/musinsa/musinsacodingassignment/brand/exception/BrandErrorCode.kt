31958cc454c00562e214d2a9c545351e102bec59
leonard1022 <dlstlrl8704@gmail.com>
2025-01-27 13:26:09 +0900

    Update: modified brand exception

--- src/main/kotlin/com/musinsa/musinsacodingassignment/brand/exception/BrandErrorCode.kt
+++ src/main/kotlin/com/musinsa/musinsacodingassignment/brand/exception/BrandErrorCode.kt
@@ -7,9 +7,4 @@ enum class BrandErrorCode(
     override val code: String, override val message: String, override val httpStatus: HttpStatus
 ) : BaseErrorCode {
     BRAND_NOT_FOUND("BRAND_001", "Brand not found", HttpStatus.NOT_FOUND),
-    BRAND_NAME_IS_REQUIRED("BRAND_002", "Brand name is required", HttpStatus.BAD_REQUEST),
-    BRAND_ALREADY_EXISTS("BRAND_003", "Brand already exists", HttpStatus.BAD_REQUEST),
-    INTERNAL_SERVER_ERROR("BRAND_004", "Internal server error", HttpStatus.INTERNAL_SERVER_ERROR),
-    BRAND_DUPLICATED("BRAND_005", "Brand Duplicated", HttpStatus.BAD_REQUEST)
-    ;
 }
