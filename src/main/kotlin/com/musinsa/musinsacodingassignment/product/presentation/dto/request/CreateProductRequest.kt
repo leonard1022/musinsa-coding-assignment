bcd09385274d06e605a3e94895e670205d991c35
leonard1022 <dlstlrl8704@gmail.com>
2025-01-27 13:26:48 +0900

    Update: modified product exception

--- src/main/kotlin/com/musinsa/musinsacodingassignment/product/presentation/dto/request/CreateProductRequest.kt
+++ src/main/kotlin/com/musinsa/musinsacodingassignment/product/presentation/dto/request/CreateProductRequest.kt
@@ -1,14 +1,14 @@
 package com.musinsa.musinsacodingassignment.product.presentation.dto.request
 
 import com.musinsa.musinsacodingassignment.product.service.vo.CreateProductVO
+import jakarta.validation.constraints.NotNull
 
 
 data class CreateProductRequest(
-    val brandId: Long,
-    val categoryId: Long,
-    val price: Int
+    @field:NotNull val brandId: Long,
+    @field:NotNull val categoryId: Long,
+    @field:NotNull val price: Int
 )
-
 
 fun CreateProductRequest.toCreateProductVO() = CreateProductVO(
     brandId = this.brandId,
