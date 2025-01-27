40ecb5c99ae24b8653313d3fd4d450a0589279f7
leonard1022 <dlstlrl8704@gmail.com>
2025-01-27 13:26:26 +0900

    Update: modified category exception

--- src/main/kotlin/com/musinsa/musinsacodingassignment/category/exception/CategoryErrorCode.kt
+++ src/main/kotlin/com/musinsa/musinsacodingassignment/category/exception/CategoryErrorCode.kt
@@ -7,9 +7,5 @@ enum class CategoryErrorCode(
     override val code: String, override val message: String, override val httpStatus: HttpStatus
 ) : BaseErrorCode {
     CATEGORY_NOT_FOUND("CATEGORY_001", "Category not found", HttpStatus.NOT_FOUND),
-    CATEGORY_NAME_IS_REQUIRED("CATEGORY_002", "Category name is required", HttpStatus.BAD_REQUEST),
     CATEGORY_ALREADY_EXISTS("CATEGORY_003", "Category already exists", HttpStatus.BAD_REQUEST),
-    INTERNAL_SERVER_ERROR("CATEGORY_004", "Internal server error", HttpStatus.INTERNAL_SERVER_ERROR),
-    CATEGORY_DUPLICATED("CATEGORY_005", "Category Duplicated", HttpStatus.BAD_REQUEST)
-    ;
 }
